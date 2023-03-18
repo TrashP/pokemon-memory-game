b6e7086d940d553232fd4aab79e7873e34592bb6
Arnab <arnabdnri@gmail.com>
2023-03-18 20:53:55 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Header from './components/Header';
+
+const App = () => {
+	return (
+		<div id="app">
+			<Header></Header>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<div id="header">
+			<div id="title">POKEMON MEMORY</div>
+			<div id="circle"></div>
+		</div>
+	);
+};
+
+export default Header;
